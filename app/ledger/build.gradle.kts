1658437e36b6aaea924ea723a0fdc8acb410cd5d
Your Name <you@example.com>
2025-01-04 18:25:35 +0100

    ehh

--- app/ledger/build.gradle.kts
+++ app/ledger/build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "2.0.20"
 }
 
 group = "com.ksidelta"
+version = "unspecified"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
 }
